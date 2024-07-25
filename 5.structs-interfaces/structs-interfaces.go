cb8901f526fb364f34fc75e82ee53d8d82ed22db
Samuel Palacios <samuelpl0888@gmail.com>
2024-07-25 18:19:20 -0400

    add: 5.3 interfaces(refactor)

--- 5.structs-interfaces/structs-interfaces.go
+++ 5.structs-interfaces/structs-interfaces.go
@@ -2,11 +2,16 @@ package structs_interfaces
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+// Method of Rectangle struct
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
 }
@@ -15,6 +20,7 @@ type Circle struct {
 	Radius float64
 }
 
+// Method of Circle struct
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
--- 5.structs-interfaces/structs-interfaces_test.go
+++ 5.structs-interfaces/structs-interfaces_test.go
@@ -3,22 +3,23 @@ package structs_interfaces
 import "testing"
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{2.75, 5}
-		got := rectangle.Area()
-		want := 13.75
-		if got != want {
-			t.Errorf("got %2.f want %2.f", got, want)
-		}
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
 
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
 
