5c5ebacf8f25efad53e3c0ed0418a94f45ac9866
Samuel Palacios <samuelpl0888@gmail.com>
2024-07-25 17:58:03 -0400

    add: 5.2 structs (con métodos)

--- 5.structs-interfaces/structs-interfaces.go
+++ 5.structs-interfaces/structs-interfaces.go
@@ -1,12 +1,22 @@
 package structs_interfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- 5.structs-interfaces/structs-interfaces_test.go
+++ 5.structs-interfaces/structs-interfaces_test.go
@@ -5,10 +5,19 @@ import "testing"
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{2.75, 5}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 13.75
 		if got != want {
 			t.Errorf("got %2.f want %2.f", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
